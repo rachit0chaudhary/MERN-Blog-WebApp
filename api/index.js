101142740b84a10c2da7dce5767290c35af0faa8
rachit <rachitchoudary@gmail.com>
2024-04-20 23:46:23 +0530

    atlas connected

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 import express from "express";
-
 const app = express();
+
+import mongoose from "mongoose";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("db is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
   console.log("app is listen on 3000");
