d91aff66321d5fd254994c15e05761ec736dc469
rachit <rachitchoudary@gmail.com>
2024-04-20 19:25:02 +0530

    header styling done

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -6,9 +6,11 @@ import SignIn from "./pages/signIn";
 import SignUp from "./pages/signUp";
 import Dashboard from "./pages/dashboard";
 import Projects from "./pages/Projects";
+import Header from "./components.jsx/header";
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
