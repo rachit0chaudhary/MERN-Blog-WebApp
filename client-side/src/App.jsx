c638cac230e09ec00270d8f24d4f1a6b58467ace
rachit <rachitchoudary@gmail.com>
2024-04-20 13:38:41 +0530

    second commit

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -1,5 +1,22 @@
 import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import About from "./pages/about";
+import SignIn from "./pages/signIn";
+import SignUp from "./pages/signUp";
+import Dashboard from "./pages/dashboard";
+import Projects from "./pages/Projects";
 export default function App() {
-  return <div className="text-3xl text-red-500">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
